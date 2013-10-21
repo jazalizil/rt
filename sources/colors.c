2b85c80ec2d9d18da2529ffbdc8c861c03d52618
jazalizil <dabbechjalil@yahoo.fr>
2013-10-21 14:00:38 +0200

    sa avance... reste plus qu'a faire les calculs

---
+++ sources/colors.c
@@ -0,0 +1,47 @@
+/*
+** colors.c for sources in /root/projets/igraph/rt/sources
+** 
+** Made by jalil dabbech
+** Login   <dabbec_j@epitech.net>
+** 
+** Started on  Mon Oct 21 02:16:51 2013 jalil dabbech
+** Last update Mon Oct 21 13:59:02 2013 jalil dabbech
+*/
+
+#include <unistd.h>
+#include "rt.h"
+
+t_color		*calc_inter(int x, int y, t_obj_list **objects, t_cam *cam)
+{
+  t_color	*ret;
+
+  while (*objects)
+  {
+    objects = &((*objects)->next);
+  }
+  return (ret);
+}
+
+t_color		get_color(int x, int y, t_obj_list **objects, t_cam *cam)
+{
+  static int	coef = 1;
+  t_v3D		rayon;
+  t_color	*ret;
+
+  if (!(ret = calc_inter(x, y, objects, cam)))
+  {
+    if (x % (coef * 20) == 0 || y % (k * 20) == 0)
+    {
+      (*ret).red = 0;
+      (*ret).green = 0xFF;
+      (*ret).blue = 0xFF;
+    }
+    else
+    {
+      (*ret).red = 0x8C;
+      (*ret).green = 0x4F;
+      (*ret).blue = 0;
+    }
+  }
+  return (*ret);
+}
---
+++ sources/intersections.c
@@ -0,0 +1,10 @@
+/*
+** intersections.c for sources in /root/projets/igraph/rt/sources
+** 
+** Made by jalil dabbech
+** Login   <dabbec_j@epitech.net>
+** 
+** Started on  Mon Oct 21 13:57:01 2013 jalil dabbech
+** Last update Mon Oct 21 13:57:03 2013 jalil dabbech
+*/
+
